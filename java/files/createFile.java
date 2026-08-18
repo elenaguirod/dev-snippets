7c51b83d9946535cd905252c5ab6ca9faffbf744
elenaguirod <guijarrorodriguez.elena@gmail.com>
2026-08-18 10:20:05 +0200

    Update and rename create-file.java to createFile.java

--- java/files/createFile.java
+++ java/files/createFile.java
@@ -1,12 +1,12 @@
+// Creates a new file if it does not already exist.
+
 try {
-    // We create the object that encapsules the file.
     File newFile = new File ("pathComplete");
-    // From the File object we create the file physically.
     if (newFile.createNewFile()) {
         System.out.println("sentence");
     } else {
         System.out.println("sentence");
     }
-} catch (Exception ioe) {
-    ioe.getMessage();
+} catch (IOException ioe) {
+    System.out.println(ioe.getMessage());
 }
