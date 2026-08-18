067959c5757e1270a48ec7280120504ba0b10548
elenaguirod <guijarrorodriguez.elena@gmail.com>
2026-08-18 10:14:55 +0200

    Update and rename normalize-path.java to normalizePath.java

--- java/files/normalizePath.java
+++ java/files/normalizePath.java
@@ -1,14 +1,14 @@
-static String pathSeparator(String path){
+// Normalizes path separators according to the current operating system.
+
+static String normalizePath(String path){
     String separatorSlash = "\\";
     try {
         // If we are on Windows
         if (File.separator.equals(separatorSlash)) {
             separatorSlash = "/";
-        // Replace all strings that have a match with the regular expression for the string File.separator.
         }
         return path.replaceAll(separatorSlash, File.separator); 
-    } catch (Exception e) {
-        // If a java.util.regex.PatternSyntaxException happens.
+    } catch (PatternSyntaxException e) {
         return path.replaceAll(separatorSlash + separatorSlash, File.separator);
     } 
 }
